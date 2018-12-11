a3c048fbe0a54b0b0fffbf08b0a5032465fdedae
Mariia Vasylenko <>
2018-12-11 12:51:11 +0200

    change while to for

--- Homework2.java
+++ Homework2.java
@@ -9,23 +9,22 @@ public class Homework2 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int[] myArray = new int[10];
-        int i = 0;
-        while (i < myArray.length) {
+        for (int i=0;i < myArray.length; i++) {
             System.out.println("Enter numbers for array");
             try {
                 myArray[i] = s.nextInt();
-                i++;
+
             } catch (InputMismatchException e) {
                 s.next();
                 System.out.println("Array should contain only numbers");
             }
         }
         System.out.println("Reverted array");
-        while (i != 0) {
-            i--;
+        for (int i = myArray.length-1; i >= 0; i--) {
             System.out.println(myArray[i]);
         }
 
     }
 
 }
+
